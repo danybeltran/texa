ab3470fa0f73895a526e50376e8c79bc7ee77f4d
danybeltran <josuebeltran_2010@hotmail.com>
2024-05-12 13:19:48 -0600

    fix(text): Fixes headings' space being shrunk

--- states/index.ts
+++ states/index.ts
@@ -3,11 +3,6 @@
 import { Doc, Folder } from '@prisma/client'
 import { atom } from 'atomic-state'
 
-export const countState = atom({
-  key: 'count',
-  default: 0
-})
-
 export const itemsToMoveState = atom<(Doc | Folder)[]>({
   key: 'itemToMove',
   default: []
